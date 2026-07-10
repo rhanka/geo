4314a65e5e2963363456d94bbc07cdb51ade1a94
rhanka <fabien.antoine@m4x.org>
2026-07-10 18:49:45 -0400

    port(geo): add pure lot zone join

---
+++ packages/geo/src/zonage/index.ts
@@ -0,0 +1,14 @@
+export {
+  assignLotZones,
+  canonicalizeZoneCodeForJoin,
+  enrichWithNorms,
+  normalizeZoneCode,
+  zoneNumberOf,
+  type LotZoneAssignment,
+  type LotZoneAssignmentMethod,
+  type LotZoneJoinOptions,
+  type LotZoneNormAssignment,
+  type NormsRecord,
+  type PolygonalFeature,
+  type PolygonalGeometry,
+} from "./lotZoneJoin.js";
