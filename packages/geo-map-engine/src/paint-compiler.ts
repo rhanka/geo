9533c21d80c048d2a2575bba8b8015f840daf1fb
rhanka <fabien.antoine@m4x.org>
2026-08-16 08:54:23 -0400

    feat(geo-map-engine): scaffold paint compiler

---
+++ packages/geo-map-engine/src/paint-compiler.ts
@@ -0,0 +1,7 @@
+/**
+ * Compilation interne des encodages renderer-neutres vers MapLibre 2D.
+ *
+ * Le contrat public reste entièrement renderer-neutre ; les expressions
+ * MapLibre ne sortent jamais de ce module.
+ */
+export {};
